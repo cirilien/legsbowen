552fa07b9696e7c061c8dcd340ea8144905b28c2
cirilien <accio.cirilien@gmail.com>
2020-03-21 13:20:56 +1100

    more pictures

--- script.js
+++ script.js
@@ -4,7 +4,29 @@ const images = [
   "huaisang3.jpg",
   "huaisang4.png",
   "huaisang5.jpg",
-  "huaisang6.jpg"
+  "huaisang7.png",
+  "huaisang8.jpg",
+  "huaisang9.jpg",
+  "huaisang10.jpg",
+  "huaisang11.jpg",
+  "huaisang12.jpg",
+  "huaisang13.jpg",
+  "huaisang14.jpg",
+  "huaisang15.jpg",
+  "huaisang16.jpg",
+  "huaisang17.jpg",
+  "huaisang18.jpg",
+  "huaisang19.png",
+  "huaisang20.jpg",
+  "huaisang21.jpg",
+  "huaisang22.jpg",
+  "huaisang23.jpg",
+  "huaisang24.jpeg",
+  "huaisang25.jpeg",
+  "huaisang26.jpg",
+  "huaisang27.jpg",
+  "huaisang28.jpg",
+  "huaisang29.jpg"
 ];
 
 function randomFromIamgesArray() {
